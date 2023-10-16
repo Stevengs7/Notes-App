a80cdd55a73ae5391e1270b11463855093a0fadd
Mario <gserna.ms@gmail.com>
2023-10-16 13:39:22 +0200

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Mario Steeven Garzon Serna on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
