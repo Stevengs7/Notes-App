9d8d756ca4449bfe71e30e31586094c0527556dc
Mario <gserna.ms@gmail.com>
2023-10-17 13:41:35 +0200

    Xcode update

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -8,17 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newNota = ""
+    @State private var allnotes: [NotaItem] = []
+
+    
+    struct NotaItem: Identifiable {
+        let id = UUID()
+        let note: String
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                TextField("Write here", text: $newNota)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                Button(action: {
+                    addNote()
+                }) {
+                    Image(systemName: "plus")
+                }
+                .padding()
+
+                List {
+                    ForEach(allnotes) { nota in
+                        HStack {
+                            Text(nota.note)
+                            Spacer()
+                            Button(action: {
+                                deleteNote(nota)
+                            }) {
+                                Image(systemName: "trash")
+                                    .foregroundColor(.red)
+                            }
+                        }
+                    }
+                }
+
+                Text("Welcome back to Notes!")
+            }
+            .padding()
+            .navigationTitle("Notes")
         }
-        .padding()
+    }
+
+// Añadir una nota a allnotes como NotaItem
+    
+    func addNote() {
+        guard !self.newNota.isEmpty else { return }
+        self.allnotes.append(NotaItem(note: self.newNota))
+        self.newNota = ""
+    }
+
+// Borrar una nota de allnotes como NotaItem por medio de su UUID
+    
+    func deleteNote(_ nota: NotaItem) {
+        if let index = allnotes.firstIndex(where: { $0.id == nota.id }) {
+            allnotes.remove(at: index)
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
+
+
+
